02c6cf1cb65ae9734d0d0d6aecfd6655ffab6a93
Keiran Hamilton <keiranhamilton@gmail.com>
2018-11-05 22:17:51 -0500

    inital site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,49 @@
 
-var myAnimals = ["giraffe", "kangaroo", "elephant", "monkey", "sloth", "panther", "hippopotamus"
+var words = ["giraffe", "kangaroo", "elephant", "monkey", "sloth", "panther", "hippopotamus"
 ];
-var word = myAnimals[Math.floor(Math.random() * myAnimals.length)];
- 
-
-
-$("#root").html(word)
-
-
- var answerList = [];
- for (var i = 0; i < word.length; i++) {
- answerList[i] = "_";
- }
- 
- var remainingLetters = word.length;
-
- 
- while (remainingLetters > 0) {
- 
- alert(answerList.join(" "));
- 
- var guess = prompt("Please guess a letter,");
- 
- if (guess === null) {
- // Exit the game loop
- break;
- 
-} else if (guess.length !== 1) {
- alert("Please enter a single letter.");
- } else {
- 
- for (var j = 0; j < myAnimals.length; j++) {
- if (word[j] === guess) {
- answerList[j] = guess;
- remainingLetters--;
- }
- }
- }
-}
-
-alert(answerList.join(" "));
-alert("Awesome! Great Job! " + word);
-
-
+var game = {
+    guessed: [],
+    left: 12,
+    start: function() {
+      this.complete = false;
+      this.word = words[Math.floor(Math.random() * words.length)];
+      this.$right = document.getElementById('right');
+      this.$wrong = document.getElementById('wrong');
+      this.$remain = document.getElementById('remain');
+      this.$right.innerHTML = '_'.repeat(this.word.length);
+    },
+    guess: function(letter) {
+      if (this.left > 0 && this.complete != true) {
+        if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
+          this.right(letter);
+        } else {
+          this.wrong(letter);
+        }}
+    },
+    right: function(letter) {
+      for(var i = 0; i < this.word.length; i++) {
+        if (this.word[i] == letter) {
+          var word = this.$right.innerHTML.split('');
+          word[i] = letter;
+          this.$right.innerHTML = word.join('');
+        }
+      }
+      if (this.$right.innerHTML.indexOf('_') < 0) {
+            alert('Awesome, Great Job!!');
+        this.complete = true;}
+    },
+    wrong: function(letter) {
+      this.guessed.push(letter);
+      this.$wrong.innerHTML += ' ' + letter;
+        this.left--;
+      this.$remain.innerHTML = this.left;
+      if (this.left < 1) {
+        alert('You lost, but keep trying until you win! '+ this.word);
+        this.complete = true;}
+    }
+  };
+  game.start();
+  document.onkeyup = function(event) {
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    game.guess(letter);
+  };
